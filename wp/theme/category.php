4aa418cae7082a43180206fbb4c63ecc09fc1988
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-07-11 20:03:06 +0300

    Wordpress theme: Simplifying template pipethroughs

--- wp/theme/category.php
+++ wp/theme/category.php
@@ -4,15 +4,9 @@ ob_start();
 
 require( get_template_directory() . "/category.php" );
 
-$output = ob_get_contents();
-
-ob_end_clean();
-
-$output = preg_replace(
+echo( preg_replace(
 	'/[<]h1 class="archive-title"[>][^:]*: *([^<]*)/',
 	'<h1 class="archive-title">$1',
-	$output );
-
-echo( $output );
+	ob_get_clean() ) );
 
 ?>
--- wp/theme/header.php
+++ wp/theme/header.php
@@ -1,17 +1,13 @@
 <?php
+// Remove pingback and EditURL <link> tags
 
 ob_start();
 
 require( get_template_directory() . "/header.php" );
 
-$contents = ob_get_contents();
-
-ob_end_clean();
-
-// Remove pingback and EditURL <link> tags
 echo( preg_replace(
 	'/[<]link rel="(pingback|EditURI)"[^>]*[>]/',
 	'',
-	$contents ) );
+	ob_get_clean() ) );
 
 ?>
