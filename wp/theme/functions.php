311357f09f357a84f19ceefe83161a4e34a62387
Gabriel Schulhof <gabriel.schulhof@intel.com>
2014-07-20 00:51:03 +0300

    Wordpress theme: Sort posts alphabetically by title

--- wp/theme/functions.php
+++ wp/theme/functions.php
@@ -4,4 +4,11 @@ function twentyfourteen_post_nav(){}
 function twentyfourteen_posted_on(){}
 
 remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+
+function wuf_modify_query_order( $query ) {
+	$query->set( 'orderby', 'title' );
+	$query->set( 'order', 'ASC' );
+	return $query;
+}
+add_action( 'pre_get_posts', 'wuf_modify_query_order' );
 ?>
